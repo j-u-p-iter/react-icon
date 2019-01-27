b678b4dfe589f0ae172eb06212d2baa474e349a7
j.u.p.iter <pn79122821224@gmail.com>
2019-01-27 11:22:15 +0500

    v0.0.1

---
+++ src/Icon.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+
+interface IProps {
+  name: string;
+  className: string;
+  onClick?: (event: React.MouseEvent<SVGElement>) => void;
+  svgSpritePath: string;
+}
+
+export const Icon: React.SFC<IProps> = ({
+  name,
+  className,
+  onClick,
+  svgSpritePath,
+}) => (
+  <svg
+    role='img'
+    name={name}
+    className={className}
+    onClick={onClick}
+    viewBox='0 0 16 16'
+  >
+    <use xlinkHref={`${svgSpritePath}#${name}`} />
+  </svg>
+);
